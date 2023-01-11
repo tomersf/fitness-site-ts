9dacc1675c9fa0f1877d88aebace732d96d8e251
Tomer Shafir <tomershafir@Tomers-MacBook-Pro.local>
2023-01-11 20:31:49 +0200

    add SelectedPage enum && fix props statments

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
   return (
     <div className="app bg-gray-20">
-      <Navbar />
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   );
 }
