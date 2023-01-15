6fe46c8416e8eb38e799db30f296a090207f799a
Tomer Shafir <tomershafir@Tomers-MacBook-Pro.local>
2023-01-15 12:09:15 +0200

    add ourClasses page

---
+++ src/scenes/ourClasses/index.tsx
@@ -0,0 +1,93 @@
+import { SelectedPage } from "@/shared/types";
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
+import { motion } from "framer-motion";
+import HText from "@/shared/HText";
+import { ClassType } from "@/shared/types";
+import Class from "@/scenes/ourClasses/Class";
+
+const classes: Array<ClassType> = [
+  {
+    name: "Weight Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: image1,
+  },
+  {
+    name: "Fitness Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: image2,
+  },
+  {
+    name: "Adventure Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: image3,
+  },
+  {
+    name: "Ab Core Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: image4,
+  },
+  {
+    name: "Yoga Classes",
+    image: image5,
+  },
+  {
+    name: "Meditation Classes",
+    image: image6,
+  },
+];
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const OurClasses = ({ setSelectedPage }: Props) => {
+  return (
+    <section className="w-full bg-primary-100 py-32" id="ourclasses">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+            <p className="py-5">
+              We have an amazing classes. Feel free to join us!
+            </p>
+          </div>
+        </motion.div>
+        <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classes.map((item, index) => (
+              <Class
+                key={`${item.name}-${index}}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default OurClasses;
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -14,3 +14,9 @@ export interface BenefitType {
   title: string;
   description: string;
 }
+
+export interface ClassType {
+  name: string;
+  description?: string;
+  image: string;
+}
